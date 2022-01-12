e91cd4c5762dcd6265ec365de17a6ddd4c4caffc
khasanboi <k.kholmatov@student.inha.uz>
2022-01-12 21:38:48 +0500

    buttons

--- buttons.py
+++ buttons.py
@@ -1,5 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-btn1 = KeyboardButton('1')
-btn2 = KeyboardButton('2')
+btn1 = KeyboardButton('USD to UZS')
+btn2 = KeyboardButton('UZS to USD')
 menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn1, btn2)
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+
+import buttons
 import settings
 from buttons import *
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
 bot = Bot(token=settings.TOKEN)
-dp = Dispatcher(bot)  # check bot for new messages and requests and connects with backend
+dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Welcome to our bot')
-
-
+    await bot.send_message(message.from_user.id, 'Welcome to our bot', reply_markup=buttons.menu)
 
 @dp.message_handler()
 async def exchange(message: types.Message):
